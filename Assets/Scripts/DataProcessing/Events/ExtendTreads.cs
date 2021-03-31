cfccf9eb34ccfc2379b9792dd20c9fd8ebe6236a
rolando-ms <rolando.moralessuarez@gmail.com>
2021-03-31 19:56:39 +0200

    Helping scripts for stairs variation

---
+++ Assets/Scripts/DataProcessing/Events/ExtendTreads.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ExtendTreads : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Ascending
+        for(int i=0; i<transform.childCount; i++)
+        {
+            Transform treadTransform = GetChildTransform(transform.GetChild(i).transform);
+            StepData treadData = treadTransform.GetComponent<StepData>();
+            StepData previousTreadData;
+            if(treadData.DefaultScale.x < 1) 
+            {
+                if(i == 0) previousTreadData = GetChildTransform(transform.GetChild(i).transform).GetComponent<StepData>();
+                else previousTreadData = GetChildTransform(transform.GetChild(i-1).transform).GetComponent<StepData>();
+
+                // Saving horizontal offset
+                if(i == 0) treadData.MaxHorizontalOffset = treadData.DefaultScale.x;
+                else if(previousTreadData.DefaultScale.x != 1) treadData.MaxHorizontalOffset = previousTreadData.DefaultScale.x;
+                else treadData.MaxHorizontalOffset = previousTreadData.OriginalXScale;
+
+                // Save original X value
+                treadData.OriginalXScale = treadData.DefaultScale.x;
+
+                // Extend tread width
+                treadData.DefaultScale.x = 1;
+                
+                // Correct Position
+                treadData.DefaultPosition -= treadTransform.right.normalized * ((1f - treadData.OriginalXScale)/2f);
+
+                // Correct Max Scale Noise
+                treadData.MaxScaleNoise.x = 1.08f;
+
+                Debug.Log("Tread " + treadTransform.name + " updated");
+            } else if(treadData.DefaultScale.x == 4)
+            {
+                previousTreadData = GetChildTransform(transform.GetChild(i-1).transform).GetComponent<StepData>();
+                treadData.MaxHorizontalOffset = previousTreadData.OriginalXScale;
+                treadData.OriginalXScale = 4;
+            }
+        }
+        
+
+        /*
+        // Descending
+        for(int i=0; i<transform.childCount; i++)
+        {
+            Transform treadTransform = GetChildTransform(transform.GetChild(i).transform);
+            StepData treadData = treadTransform.GetComponent<StepData>();
+            StepData nextTreadData;
+            if(treadData.DefaultScale.x < 1) 
+            {
+                if(i == transform.childCount - 1) nextTreadData = GetChildTransform(transform.GetChild(i).transform).GetComponent<StepData>();
+                else nextTreadData = GetChildTransform(transform.GetChild(i+1).transform).GetComponent<StepData>();
+
+                // Saving horizontal offset
+                if(i == transform.childCount) treadData.MaxHorizontalOffset = treadData.DefaultScale.x;
+                else if(nextTreadData.DefaultScale.x != 1) treadData.MaxHorizontalOffset = nextTreadData.DefaultScale.x;
+                else treadData.MaxHorizontalOffset = nextTreadData.OriginalXScale;
+
+                // Save original X value
+                treadData.OriginalXScale = treadData.DefaultScale.x;
+
+                // Extend tread width
+                treadData.DefaultScale.x = 1;
+                
+                // Correct Position
+                treadData.DefaultPosition -= treadTransform.right.normalized * ((1f - treadData.OriginalXScale)/2f);
+
+                // Correct Max Scale Noise
+                treadData.MaxScaleNoise.x = 1.08f;
+
+                Debug.Log("Tread " + treadTransform.name + " updated");
+            } else if(treadData.DefaultScale.x == 4)
+            {
+                nextTreadData = GetChildTransform(transform.GetChild(i+1).transform).GetComponent<StepData>();
+                if(nextTreadData.DefaultScale.x != 1) treadData.MaxHorizontalOffset = nextTreadData.DefaultScale.x;
+                treadData.OriginalXScale = 4;
+            }
+        }
+        */    
+    }
+
+    private Transform GetChildTransform(Transform childTransform){
+        if(childTransform.childCount == 0) return childTransform;
+        else return GetChildTransform(childTransform.GetChild(0));
+    }
+}
---
+++ Assets/Scripts/DataProcessing/Events/ReplaceDeleteStepComponents.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ReplaceDeleteStepComponents : MonoBehaviour
+{
+    void Update()
+    {
+        for(int i=0; i < transform.childCount; i++)
+        {
+            Transform childTransform = GetChildTransform(transform.GetChild(i));
+            if(childTransform.GetComponent<StepData>() == null)
+            {
+                childTransform.gameObject.AddComponent<StepData>();
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        for(int i=0; i < transform.childCount; i++)
+        {
+            Transform childTransform = GetChildTransform(transform.GetChild(i));
+            if(childTransform.GetComponent<NoiseSteps>() != null && childTransform.GetComponent<StepData>().DefaultScale.x != 1f)
+            {
+                DestroyImmediate(childTransform.GetComponent<NoiseSteps>());
+            }
+        }
+        for(int i=0; i < transform.childCount; i++)
+        {
+            Transform childTransform = GetChildTransform(transform.GetChild(i));
+            if(childTransform.GetComponent<StepData>().MaxScaleNoise.x != 1.25f)
+            {
+                childTransform.GetComponent<StepData>().MaxScaleNoise.x = 1.25f;
+            }
+            if(childTransform.GetComponent<StepData>().DefaultScale.y == 4f)
+            {
+                childTransform.GetComponent<StepData>().DefaultScale.y = 8f;
+                childTransform.GetComponent<StepData>().DefaultPosition.y -= 2f;
+            }
+            if(childTransform.tag != "Tread")
+            {
+                childTransform.tag = "Tread";
+            }
+        }
+    }
+
+    void OnRemoveComponent()
+    {
+        for(int i=0; i < transform.childCount; i++)
+        {
+            Transform childTransform = GetChildTransform(transform.GetChild(i));
+            //childTransform.gameObject.AddComponent<StepData>();
+            Destroy(childTransform.GetComponent<NoiseSteps>());
+        }
+    }
+
+    private Transform GetChildTransform(Transform childTransform){
+        if(childTransform.childCount == 0) return childTransform;
+        else return GetChildTransform(childTransform.GetChild(0));
+    }
+}
